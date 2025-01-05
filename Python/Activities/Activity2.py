c01709f674f565abb37a0ba6095983a78108986e
Vidmural <143474385+Vidmural@users.noreply.github.com>
2025-01-06 00:50:39 +0530

    Add files via upload

---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,10 @@
+inputNum = int(input("Enter a number: "))
+
+if (inputNum % 2) == 0:
+
+    print(str(inputNum) + " is an even number")
+else:
+    print(str(inputNum) + " is an odd number")
+    
+    
+    
